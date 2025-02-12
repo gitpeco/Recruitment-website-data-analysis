33e5b7093216e4eeedc4e6194a7dec77f02db76d
gitpeco <znm9110@163.com>
2025-02-12 23:58:32 +0800

    编写添加客户函数，修改客户信息函数，以及删除创建app时不用的文件

--- Recruitment_website_data_analysis/mgr/customer.py
+++ Recruitment_website_data_analysis/mgr/customer.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 import json
+from common.models import customer
 
 def dispatcher(request):
     # 将请求参数统一放入request 的 params 属性中，方便后续处理
@@ -27,3 +28,57 @@ def dispatcher(request):
 
     else:
         return JsonResponse({'ret': 1, 'msg': '不支持该类型http请求'})
+
+def listcustomers(request):
+    # 返回一个 QuerySet 对象 ，包含所有的表记录
+    qs = customer.objects.values()
+
+    # 将 QuerySet 对象 转化为 list 类型
+    # 否则不能 被 转化为 JSON 字符串
+    retlist = list(qs)
+
+    return JsonResponse({'ret': 0, 'retlist': retlist})
+
+def addcustomer(request):
+
+    info    = request.params['data']
+
+    # 从请求消息中 获取要添加客户的信息
+    # 并且插入到数据库中
+    # 返回值 就是对应插入记录的对象
+    record = Customer.objects.create(name=info['name'] ,
+                            phonenumber=info['phonenumber'] ,
+                            address=info['address'])
+
+
+    return JsonResponse({'ret': 0, 'id':record.id})
+
+def modifycustomer(request):
+
+    # 从请求消息中 获取修改客户的信息
+    # 找到该客户，并且进行修改操作
+
+    customerid = request.params['id']
+    newdata    = request.params['newdata']
+
+    try:
+        # 根据 id 从数据库中找到相应的客户记录
+        customer = Customer.objects.get(id=customerid)
+    except Customer.DoesNotExist:
+        return  {
+                'ret': 1,
+                'msg': f'id 为`{customerid}`的客户不存在'
+        }
+
+
+    if 'name' in  newdata:
+        customer.name = newdata['name']
+    if 'phonenumber' in  newdata:
+        customer.phonenumber = newdata['phonenumber']
+    if 'address' in  newdata:
+        customer.address = newdata['address']
+
+    # 注意，一定要执行save才能将修改信息保存到数据库
+    customer.save()
+
+    return JsonResponse({'ret': 0})
